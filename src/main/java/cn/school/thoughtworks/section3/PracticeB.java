6032ce0c88de2f1bf0c24d24e449e109c4629af7
nn <npr_0815@163.com>
2019-05-17 02:18:25 +0800

    finished section3/PracticeB.java

--- src/main/java/cn/school/thoughtworks/section3/PracticeB.java
+++ src/main/java/cn/school/thoughtworks/section3/PracticeB.java
@@ -7,6 +7,11 @@ public class PracticeB {
     Map<String,Integer> createUpdatedCollection(Map<String,Integer> collectionA, Map<String,List<String>> object) {
         //实现练习要求，并改写该行代码。
 
-        return null;
+        for (String str : object.get("value")){
+            if (collectionA.containsKey(str)){
+                collectionA.put(str, collectionA.get(str)-(collectionA.get(str)/3));
+            }
+        }
+        return collectionA;
     }
 }
