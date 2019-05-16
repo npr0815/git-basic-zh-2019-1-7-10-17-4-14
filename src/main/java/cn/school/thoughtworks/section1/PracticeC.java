c7da7422f8183c8112192b5a8196c63e836f1d37
nn <npr_0815@163.com>
2019-05-17 02:16:13 +0800

    finished section1/PracticeC.java

--- src/main/java/cn/school/thoughtworks/section1/PracticeC.java
+++ src/main/java/cn/school/thoughtworks/section1/PracticeC.java
@@ -1,11 +1,15 @@
 package cn.school.thoughtworks.section1;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 public class PracticeC {
     List<String> collectSameElements(List<String> collection1, Map<String,List<String>> collection2) {
         //实现练习要求，并改写该行代码。
-        return null;
+        List<String> list1 = new ArrayList<String>(collection1);
+        list1.retainAll(collection2.get("value"));
+
+        return list1;
     }
 }
